c0d07623f3bbef31dfb64a8e600d1daf00be200d
Михаил Гуренков <m.gurenkov84@gmail.com>
2020-02-02 00:16:09 +0700

    first

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,28 @@
+import pyodbc
+
+driver = '{ODBC Driver 17 for SQL Server}'
+server = '(localdb)\MSSQLLocalDB'
+db = 'DebitCard_Data'
+user = ''
+pw = ''
+
+# con = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+pw)
+con = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+db)
+cur = con.cursor()
+try:
+    cur.execute('''
+        SELECT * 
+        FROM DC_Balance 
+        WHERE Account like '54784_'
+    ''')
+
+# Показываем результат.
+    result = cur.fetchall()
+    for item in result:
+        if item[2]<=10: print(item[0],' ',item[2])
+except pyodbc.DatabaseError as err:
+    print('Error: ',err)
+
+# Завершаем подключение.
+cur.close()
+con.close()
